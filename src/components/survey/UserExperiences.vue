1b55edcaa130f1afa3ba2c41aba2d29e7f58d084
RebeccaHA <rebeccahunt034@gmail.com>
2021-02-05 15:36:29 +0000

    set up get and post request with axios, connected to the firebase backend

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -19,10 +21,38 @@
 
 <script>
 import SurveyResult from './SurveyResult.vue';
+import axios from 'axios';
 
 export default {
   components: {
     SurveyResult
+  },
+  data() {
+    return {
+      results: []
+    };
+  },
+  methods: {
+    loadExperiences() {
+      axios
+        .get(
+          'https://vue-backend-3fdbc-default-rtdb.firebaseio.com/surveys.json'
+        )
+        .then(resp => {
+          if (resp.ok) {
+            return resp.data;
+          }
+          const results = [];
+          for (const id in resp.data) {
+            results.push({
+              id: id,
+              name: resp.data[id].name,
+              rating: resp.data[id].rating
+            });
+          }
+          this.results = results;
+        });
+    }
   }
 };
 </script>
